a992b700c150e19611dcf79ab53a1a97c4222231
sistemaderegistrodehoras <ericksimosa@gmail.com>
2025-11-09 18:11:26 -0400

    Corregir vite.config para que encuentre la carpeta public

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   server: {
     host: true
-  }
+  },
+  // Le decimos a Vite dónde están nuestros archivos públicos
+  publicDir: 'public' 
 });
